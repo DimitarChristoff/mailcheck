8a0eaf95e2f6c69071c11fb706814c20aba5df9c
DimitarChristoff <christoff@gmail.com>
2012-03-22 12:48:35 +0000

    path for mootools lib

--- test/buster.js
+++ test/buster.js
@@ -3,7 +3,7 @@ var config = exports;
 config["Browser tests"] = {
     rootPath: "../",
     environment: "browser", // or "node"
-    libs: ['./lib/mootools-yui-compressed.js'],
+    libs: ['test/lib/mootools-yui-compressed.js'],
     sources: [
         "src/*.js"
     ],
